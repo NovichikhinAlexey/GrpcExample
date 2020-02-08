64adae01b4236d04bbe35d14c8a712e7142c9a3b
Alexey <fieldid@gmail.com>
2020-02-09 00:40:38 +0200

    update

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService1;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var resp = await client.SayHelloAsync(new HelloRequest() {Name = "Alex"});
+
+            Console.WriteLine(resp.Message);
+
+            using (var stream = client.GetNumbers(new GetNumbersRequest() { Count = 10 }))
+            {
+                while (await stream.ResponseStream.MoveNext())
+                {
+                    var num = stream.ResponseStream.Current;
+                    Console.WriteLine($"{num.Index} = {num.Number}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
